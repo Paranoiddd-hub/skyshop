3842cab209f81cbdc6b58b4461b4ca97ac150fb9
Aidar <zeekacoola@gmail.com>
2025-12-26 02:54:48 +0300

    Initial commit2

---
+++ skyshop/src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -0,0 +1,23 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.search.SearchResult;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class SearchService {
+    private final StorageService storageService;
+
+    public SearchService(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    public List<SearchResult> search(String searchWord) {
+        return storageService.getSearchables().stream()
+                .filter(s -> s.getSearchTerm().contains(searchWord))
+                .map(SearchResult::fromSearchable)
+                .toList();
+    }
+}
